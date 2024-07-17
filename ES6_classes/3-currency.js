4edb4e6fd4c059bba33ab74cc11366e65fedc179
GuerinTifenn <8807@holbertonstudents.com>
2024-07-17 13:15:31 +0200

    Update task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,30 +1,23 @@
 export default class Currency {
   constructor(code, name) {
-    this._code = this.validateString(code, 'Code');
-    this._name = this.validateString(name, 'Name');
-  }
-
-  validateString(value, attribute) {
-    if (typeof value !== 'string') {
-      throw new TypeError(`${attribute} must be a string`);
-    }
-    return value;
+    this._code = code;
+    this._name = name;
   }
 
   get code() {
     return this._code;
   }
 
-  set code(value) {
-    this._code = this.validateString(value, 'Code');
+  set code(newcode) {
+    this._code = newcode;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this._name = this.validateString(value, 'Name');
+  set name(newname) {
+    this._name = newname;
   }
 
   displayFullCurrency() {
