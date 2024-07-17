7bbda7ef3cce1edaf99372e9be65f1e6a6e65734
GuerinTifenn <8807@holbertonstudents.com>
2024-07-17 16:06:20 +0200

    Update task 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,49 +1,38 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
-export default class Pricing {
+export default class Princing {
   constructor(amount, currency) {
-    this._amount = this.validateNumber(amount, 'Amount');
-    this._currency = this.validateCurrency(currency, 'Currency');
-  }
-
-  validateNumber(value, attribute) {
-    if (typeof value !== 'number') {
-      throw new TypeError(`${attribute} must be a number`);
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
     }
-    return value;
-  }
-
-  validateCurrency(value, attribute) {
-    if (!(value instanceof Currency)) {
-      throw new TypeError(`${attribute} must be an instance of Currency`);
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
     }
-    return value;
+    this._amount = amount;
+    this._currency = currency;
   }
 
   get amount() {
     return this._amount;
   }
 
-  set amount(value) {
-    this._amount = this.validateNumber(value, 'Amount');
+  set amount(newamount) {
+    this._amount = newamount;
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(value) {
-    this._currency = this.validateCurrency(value, 'Currency');
+  set currency(newcurrency) {
+    this._currency = newcurrency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
   }
 
   static convertPrice(amount, conversionRate) {
-    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
-      throw new TypeError('Both amount and conversionRate must be numbers');
-    }
     return amount * conversionRate;
   }
 }
