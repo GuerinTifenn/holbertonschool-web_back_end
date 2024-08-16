0a15287982e2944184288dee3a81a8ce8f8f1585
GuerinTifenn <8807@holbertonstudents.com>
2024-08-16 10:11:31 +0200

    update pycodestyle

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -8,6 +8,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn task_wait_random n times with the specified max_delay.
