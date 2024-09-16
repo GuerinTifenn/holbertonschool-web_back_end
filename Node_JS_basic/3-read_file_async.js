31c21173e5bbb653e89cd4b588e9756549034442
GuerinTifenn <8807@holbertonstudents.com>
2024-09-16 13:40:53 +0200

    Add task 3

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (error, data) => {
+            if (error) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
+            const lines = data.trim().split('\n').filter(line => line);
+            const students = lines.slice(1);
+            const fields = {};
+
+            students.forEach((student) => {
+                const [firstname, lastname, age, field] = student.split(',');
+                if (!fields[field]) {
+                    fields[field] = [];
+                }
+                fields[field].push(firstname);
+            });
+
+            console.log(`Number of students: ${students.length}`);
+            for (const field in fields) {
+                console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+            }
+            resolve();
+        });
+    });
+}
+
+module.exports = countStudents;
