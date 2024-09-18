8ac6de5f7a930f141fa0bd485fc78a33232eaea9
GuerinTifenn <8807@holbertonstudents.com>
2024-09-18 09:42:38 +0200

    Update 4-5

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,15 +1,20 @@
 const http = require('http');
 
-const port = '1245';
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-const requestListener = function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!');
-};
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
 
-const app = http.createServer(requestListener);
-app.listen(port, () => {
-  console.log('...');
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
 
 module.exports = app;
